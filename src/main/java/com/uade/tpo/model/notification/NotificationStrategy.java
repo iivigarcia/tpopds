169a4ca6163226140e88c89f7f4235bdd7c6e559
ivan <43122250+iivigarcia@users.noreply.github.com>
2025-06-26 23:01:29 -0300

    Se eliminan comentarios

--- src/main/java/com/uade/tpo/model/notification/NotificationStrategy.java
+++ src/main/java/com/uade/tpo/model/notification/NotificationStrategy.java
@@ -4,16 +4,7 @@ import com.uade.tpo.model.Partido;
 
 public interface NotificationStrategy {
     
-    /**
-     * Envía una notificación para un evento específico
-     * @param partido El partido relacionado con el evento
-     * @param eventType El tipo de evento (PARTIDO_CREADO, PARTIDO_ARMADO, etc.)
-     */
     void sendNotification(Partido partido, String eventType);
-    
-    /**
-     * Obtiene el nombre de la estrategia
-     * @return Nombre de la estrategia
-     */
+
     String getStrategyName();
 } 
